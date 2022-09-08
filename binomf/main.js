c4f7371f042ba7929b37f25e95169c284ba870c9
jthakur02 <jthakur@uncc.edu>
2022-09-08 18:19:52 -0400

    finish reduction functionality

--- binomf/main.js
+++ binomf/main.js
@@ -34,7 +34,24 @@ window.onload = function()
         for (var j = 1; j < k_values.length; j++) {
             str += " + " + k_values[j];
         }
+        var fstr = "";
+        for (var j = 0; j < k_values.length; j++) {
+            k_values[j]=k_values[j].replace("1a^{0}b^{0}","1");
+            k_values[j]=k_values[j].replace("1a","a");
+            k_values[j]=k_values[j].replace("1b","b");
+            k_values[j]=k_values[j].replace("a^{0}","");
+            k_values[j]=k_values[j].replace("b^{0}","");
+            k_values[j]=k_values[j].replace("a^{1}","a");
+            k_values[j]=k_values[j].replace("b^{1}","b");
+            if (j==0){
+                fstr = k_values[j];
+            }
+            else {
+                fstr += " + " + k_values[j];
+            }
+        }
         document.getElementById("ye").innerHTML = str;
+        document.getElementById("yf").innerHTML = fstr;
         MathJax.typeset();
     }
 }
